98b96b3b13a72c9c58a79d7df3f083ab1aadb6be
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-08-12 14:53:16 +0300

    updates

--- index.js
+++ index.js
@@ -10,39 +10,45 @@ const SITES = Object.fromEntries
 	(Object.entries(CONF.sites)
 	.map(x => [x[0], require(`./sites/${x[1]}.js`)]))
 
-function main()
-{	if (cluster.isWorker) start_servers()
-	else repeat(cluster.fork, CPUS) }
+function main() {
+    if (cluster.isWorker) start_servers()
+	else repeat(cluster.fork, CPUS)
+}
 
-function start_servers()
-{	const parts = CONF.listen.split(':')
+function start_servers() {
+	const parts = CONF.listen.split(':')
 	const host = parts[0]
 	const port = parseFloat(parts[1])
 
 	if (CONF.ssl === false)
 		http_server({ router: route, host, port })
-	else
-	{	http_server
-		({	router: route,
+	else {
+		http_server({
+			router: route,
 			host,
 			port,
 			key: fs.readFileSync(CONF.ssl.key, 'utf8'),
-			cert: fs.readFileSync(CONF.ssl.cert, 'utf8'), })
-		http_server({ router: redirect, host, port }) }}
+			cert: fs.readFileSync(CONF.ssl.cert, 'utf8'),
+		})
+		http_server({ router: redirect, host, port: port+1 })
+}}
 
-function route(request)
-{	const f = SITES[request.headers.host]
-	return f ? f(request) : Promise.resolve
-		({	status: 404,
-			mimetype: 'text/plain',
-			headers: [],
-			data: 'Not found' }) }
-
-const redirect = request => Promise.resolve
-	({	status: 307,
+function route(request) {
+	const f = SITES[request.headers.host]
+	return f ? f(request) : Promise.resolve({
+		status: 404,
 		mimetype: 'text/plain',
-		data: 'Redirect',
-		headers: [[ 'Location', request.headers.host + request.url ]] })
+		headers: [],
+		data: 'Not found',
+	})
+}
+
+const redirect = request => Promise.resolve({
+	status: 307,
+	mimetype: 'text/plain',
+	data: 'Redirect',
+	headers: [[ 'Location', request.headers.host + request.url ]]
+})
 
 function repeat(f, n) { for (let i = 0; i < n; i++) f() }
 
